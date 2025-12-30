06c547d952b8eac4fe7e4da42961b9baf01a5c71
OmegaSleepy <martingeorgiev2307@gmail.com>
2025-12-30 17:29:07 +0200

    Innit

---
+++ src/main/java/omegasleepy/florist/Florist_fabric.java
@@ -0,0 +1,16 @@
+package omegasleepy.florist;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Florist_fabric implements ModInitializer {
+	public static final String MOD_ID = "florist_fabric";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/omegasleepy/florist/mixin/FloristModClient.java
@@ -0,0 +1,10 @@
+package omegasleepy.florist.mixin;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FloristModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient () {
+
+    }
+}
